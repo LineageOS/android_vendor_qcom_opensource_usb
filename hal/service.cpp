8d5b22dad6d934f973a3c3e474e902529a2b4efe
Jack Pham <jackp@codeaurora.org>
2019-02-07 18:45:24 -0800

    Add USB HAL implementation
    Add implementation for IUsb (not to be confused with IUsbGadget)
    HAL which provides abstraction for querying and notification of
    USB port status changes. It is primarily meant for Type-C ports
    that support changing roles dynamically.
    
    This implements version 1.1 of IUsb. Unlike the default 1.0
    implementation bundled with AOSP, this version supports getting
    port status using the Type-C connector class interface found
    in upstream kernel.
    
    This code is adopted from
    https://android.googlesource.com/device/google/wahoo/+/refs/heads/master/usb/
    
    Change-Id: I43c5e0214989d6ce650fbacb23d1bb0b592f2dfc

--- hal/service.cpp
+++ hal/service.cpp
@@ -20,6 +20,7 @@
 #define LOG_TAG "android.hardware.usb@1.1-service-qti"
 
 #include <hidl/HidlTransportSupport.h>
+#include "Usb.h"
 #include "UsbGadget.h"
 
 using android::sp;
@@ -29,6 +30,8 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
+using android::hardware::usb::V1_1::IUsb;
+using android::hardware::usb::V1_1::implementation::Usb;
 using android::hardware::usb::gadget::V1_0::IUsbGadget;
 using android::hardware::usb::gadget::V1_0::implementation::UsbGadget;
 
@@ -36,19 +39,27 @@ using android::OK;
 using android::status_t;
 
 int main() {
-  android::sp<IUsbGadget> service = new UsbGadget();
+  android::sp<IUsb> service = new Usb();
+  android::sp<IUsbGadget> service2 = new UsbGadget();
 
-  configureRpcThreadpool(1, true /*callerWillJoin*/);
+  configureRpcThreadpool(2, true /*callerWillJoin*/);
   status_t status = service->registerAsService();
+
+  if (status != OK) {
+    ALOGE("Cannot register USB HAL service");
+    return 1;
+  }
+
+  status = service2->registerAsService();
 
   if (status != OK) {
     ALOGE("Cannot register USB Gadget HAL service");
     return 1;
   }
 
-  ALOGI("USB Gadget HAL Ready.");
+  ALOGI("QTI USB HAL Ready.");
   joinRpcThreadpool();
-  // Under noraml cases, execution will not reach this line.
-  ALOGI("USB Gadget HAL failed to join thread pool.");
+  // Under normal cases, execution will not reach this line.
+  ALOGI("QTI USB HAL failed to join thread pool.");
   return 1;
 }
