06ef6d7d5c22777393a538a3416a93112d22193d
Udipto Goswami <ugoswami@codeaurora.org>
2020-03-04 14:44:10 +0530

    USB: Adding Product specific support for USB HAL
    Now that the vendor_product.mk is picked up by more targets
    we need to distinguish those that are not using the USB HAL
    1.1. This version of the USB HAL is not intended to be used
    on targets with kernels older than 4.19 (since it relies on
    Type-C Class to be enabled).
    
    Fix this by conditionally checking the target kernel version
    when adding the HAL module to PRODUCT_PACKAGES.
    
    Change-Id: I3bfe14aed122f20607d63310a739103f0566e4e7

--- vendor_product.mk
+++ vendor_product.mk
@@ -4,7 +4,9 @@ else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.diag.func.name=diag
 endif
 
-PRODUCT_PACKAGES += android.hardware.usb@1.1-service-qti
+ifneq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),4.9 4.14))
+  PRODUCT_PACKAGES += android.hardware.usb@1.1-service-qti
+endif
 
 ifeq ($(TARGET_USES_USB_GADGET_HAL), true)
   PRODUCT_PACKAGES += android.hardware.usb.gadget@1.0-service-qti
