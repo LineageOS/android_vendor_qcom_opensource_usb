30a53a7bbb2598521a3db9e7592f35df05f81ec2
Rohith Kollalsi <quic_rkollals@quicinc.com>
2022-04-14 17:36:39 +0530

    init.qcom.usb: Set default composition to 90DB for parrot
    Modify default composition to 90DB for the parrot build target.
    
    Change-Id: If0b1cf0ed6b0c55bde44102635262c5b50c91a51

--- etc/init.qcom.usb.sh
+++ etc/init.qcom.usb.sh
@@ -100,7 +100,7 @@ if [ "$(getprop persist.vendor.usb.config)" == "" -a "$(getprop ro.build.type)" 
 	              "sdm845" | "sdm710")
 		          setprop persist.vendor.usb.config diag,serial_cdev,rmnet,dpl,adb
 		      ;;
-	              "msmnile" | "sm6150" | "trinket" | "lito" | "atoll" | "bengal" | "lahaina" | "holi" | "taro")
+	              "msmnile" | "sm6150" | "trinket" | "lito" | "atoll" | "bengal" | "lahaina" | "holi" | "taro" | "parrot")
 			  setprop persist.vendor.usb.config diag,serial_cdev,rmnet,dpl,qdss,adb
 		      ;;
 	              *)
