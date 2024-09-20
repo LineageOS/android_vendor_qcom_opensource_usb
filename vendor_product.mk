18a7494398bef50a34a391d66ffe543d1e5eb80b
Elson Roy Serrao <quic_eserrao@quicinc.com>
2024-09-20 08:48:58 -0700

    usb: vendor_product.mk: Enable USB Gadget HAL on Canoe
    Enable USB Gadget HAL on Canoe target.
    
    Change-Id: I31b60e29061871a5cff6db952bb65d15e094eea1

--- vendor_product.mk
+++ vendor_product.mk
@@ -66,7 +66,7 @@ else
   # USB Gadget HAL is enabled on newer targets and takes the place
   # of the init-based configfs rules for setting USB compositions
   #
-  ifneq ($(filter bengal kona seraph niobe taro anorak neo kalama pineapple sun parrot blair monaco trinket crow pitti volcano anorak61 neo61,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter bengal kona seraph niobe taro anorak neo kalama pineapple sun canoe parrot blair monaco trinket crow pitti volcano anorak61 neo61,$(TARGET_BOARD_PLATFORM)),)
     PRODUCT_PROPERTY_OVERRIDES += vendor.usb.use_gadget_hal=1
     PRODUCT_PACKAGES += android.hardware.usb.gadget-service.qti
     PRODUCT_PACKAGES += usb_compositions.conf
